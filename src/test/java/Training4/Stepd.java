ebab088e138ae82132e4d737b4202ce37d475326
A08019DIRP_C2X.08.05 <A08019DIRP_C2X.08.05@CDC2-D-5K0NXH2.dir.svc.accenture.com>
2019-09-20 15:18:24 +0530

    Driver_update

--- src/test/java/Training4/Stepd.java
+++ src/test/java/Training4/Stepd.java
@@ -1,5 +1,7 @@
 package Training4;
 
+import static org.junit.Assert.assertEquals;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -12,21 +14,23 @@ public class Stepd {
 	@Given("Open the browser")
 	public void open_the_browser() {
 			
-		System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\a08019dirp_c2x.08.05\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
 		driver = new ChromeDriver();
 		
 	}
 
 	@And("Enter the URL")
 	public void enter_the_URL() {
+		//driver.wait(100);
 	    
 		driver.get("https:\\www.google.com");
 	}
 
 	@Then("Homepage is displayed")
 	public void homepage_is_displayed() {
-	    
-		
+		 assertEquals("Googles",driver.getTitle());
+		 driver.close();
 	}
+
 
 }
